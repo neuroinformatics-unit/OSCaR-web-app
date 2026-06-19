fdb29a89afb305549548666026eb9249cc80c334
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-06-19 10:04:38 +0100

    add optimiser app

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,7 +82,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "oscar_web_app.users",
-    # Your stuff: custom apps go here
+    "oscar_web_app.optimser",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
