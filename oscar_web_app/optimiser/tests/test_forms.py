238c9f77819463bc11646a4f47a6e69b4dfb80a7
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-08-13 17:09:02 +0100

    group valid and invalid form tests

--- oscar_web_app/optimiser/tests/test_forms.py
+++ oscar_web_app/optimiser/tests/test_forms.py
@@ -1,91 +1,122 @@
+import pytest
 from pytest_django.asserts import assertFormSetError
 
 from oscar_web_app.optimiser.forms import GenotypeFormSet
 
-# ruff: noqa: PLR2004
 
-
-def test_valid_genotype_formset():
-    data = {
-        "form-TOTAL_FORMS": 2,
-        "form-INITIAL_FORMS": 0,
-        "form-MIN_NUM_FORMS": 1,
-        "form-MAX_NUM_FORMS": 1000,
-        "form-0-Mut-A": "WT",
-        "form-0-count": 5,
-        "form-1-Mut-A": "HET",
-        "form-1-count": 10,
-    }
-
+@pytest.mark.parametrize(
+    ("formset_data", "mutation_names"),
+    [
+        pytest.param(
+            {
+                "form-TOTAL_FORMS": 2,
+                "form-INITIAL_FORMS": 0,
+                "form-MIN_NUM_FORMS": 1,
+                "form-MAX_NUM_FORMS": 1000,
+                "form-0-Mut-A": "WT",
+                "form-0-count": 5,
+                "form-1-Mut-A": "HET",
+                "form-1-count": 10,
+            },
+            ["Mut-A"],
+            id="1 mutation",
+        ),
+        pytest.param(
+            {
+                "form-TOTAL_FORMS": 2,
+                "form-INITIAL_FORMS": 0,
+                "form-MIN_NUM_FORMS": 1,
+                "form-MAX_NUM_FORMS": 1000,
+                "form-0-Mut-A": "HET",
+                "form-0-count": 5,
+                "form-1-Mut-A": "HET",
+                "form-1-count": 20,
+                "form-1-DELETE": "on",
+            },
+            ["Mut-A"],
+            id="1 mutation - duplicate genotypes marked for deletion",
+        ),
+        pytest.param(
+            {
+                "form-TOTAL_FORMS": 2,
+                "form-INITIAL_FORMS": 0,
+                "form-MIN_NUM_FORMS": 1,
+                "form-MAX_NUM_FORMS": 1000,
+                "form-0-Mut-A": "WT",
+                "form-0-Mut-B": "HET",
+                "form-0-count": 5,
+                "form-1-Mut-A": "HET",
+                "form-1-Mut-B": "HET",
+                "form-1-count": 10,
+            },
+            ["Mut-A", "Mut-B"],
+            id="2 mutations",
+        ),
+        pytest.param(
+            {
+                "form-TOTAL_FORMS": 2,
+                "form-INITIAL_FORMS": 0,
+                "form-MIN_NUM_FORMS": 1,
+                "form-MAX_NUM_FORMS": 1000,
+                "form-0-Mut-A": "WT",
+                "form-0-Mut-B": "HET",
+                "form-0-count": 5,
+                "form-1-Mut-A": "WT",
+                "form-1-Mut-B": "HET",
+                "form-1-count": 10,
+                "form-1-DELETE": "on",
+            },
+            ["Mut-A", "Mut-B"],
+            id="2 mutations - duplicate genotypes marked for deletion",
+        ),
+    ],
+)
+def test_valid_genotype_formsets(formset_data, mutation_names):
     formset = GenotypeFormSet(
-        data,
-        form_kwargs={"mutation_names": ["Mut-A"]},
+        formset_data,
+        form_kwargs={"mutation_names": mutation_names},
     )
     assert formset.is_valid()
-    assert len(formset) == 2
 
 
-def test_not_enough_genotype_forms():
-    data = {
-        "form-TOTAL_FORMS": 1,
-        "form-INITIAL_FORMS": 0,
-        "form-MIN_NUM_FORMS": 1,
-        "form-MAX_NUM_FORMS": 1000,
-        "form-0-Mut-A": "WT",
-        "form-0-count": 1,
-        "form-0-DELETE": "on",
-    }
-
+@pytest.mark.parametrize(
+    ("formset_data", "mutation_names", "expected_msg"),
+    [
+        pytest.param(
+            {
+                "form-TOTAL_FORMS": 1,
+                "form-INITIAL_FORMS": 0,
+                "form-MIN_NUM_FORMS": 1,
+                "form-MAX_NUM_FORMS": 1000,
+                "form-0-Mut-A": "WT",
+                "form-0-count": 1,
+                "form-0-DELETE": "on",
+            },
+            ["Mut-A"],
+            "Please submit at least 1 form.",
+            id="All forms marked deleted",
+        ),
+        pytest.param(
+            {
+                "form-TOTAL_FORMS": 2,
+                "form-INITIAL_FORMS": 0,
+                "form-MIN_NUM_FORMS": 1,
+                "form-MAX_NUM_FORMS": 1000,
+                "form-0-Mut-A": "HET",
+                "form-0-count": 5,
+                "form-1-Mut-A": "HET",
+                "form-1-count": 20,
+            },
+            ["Mut-A"],
+            "All genotypes must be unique",
+            id="1 mutation - duplicate genotypes",
+        ),
+    ],
+)
+def test_invalid_genotype_formsets(formset_data, mutation_names, expected_msg):
     formset = GenotypeFormSet(
-        data,
-        form_kwargs={"mutation_names": ["Mut-A"]},
+        formset_data,
+        form_kwargs={"mutation_names": mutation_names},
     )
 
-    assertFormSetError(
-        formset, form_index=None, field=None, errors=["Please submit at least 1 form."]
-    )
-
-
-def test_duplicate_genotypes():
-    data = {
-        "form-TOTAL_FORMS": 2,
-        "form-INITIAL_FORMS": 0,
-        "form-MIN_NUM_FORMS": 1,
-        "form-MAX_NUM_FORMS": 1000,
-        "form-0-Mut-A": "HET",
-        "form-0-count": 5,
-        "form-1-Mut-A": "HET",
-        "form-1-count": 20,
-    }
-
-    formset = GenotypeFormSet(
-        data,
-        form_kwargs={"mutation_names": ["Mut-A"]},
-    )
-
-    assertFormSetError(
-        formset, form_index=None, field=None, errors=["All genotypes must be unique"]
-    )
-
-
-def test_duplicate_genotypes_no_deleted():
-    data = {
-        "form-TOTAL_FORMS": 2,
-        "form-INITIAL_FORMS": 0,
-        "form-MIN_NUM_FORMS": 1,
-        "form-MAX_NUM_FORMS": 1000,
-        "form-0-Mut-A": "HET",
-        "form-0-count": 5,
-        "form-1-Mut-A": "HET",
-        "form-1-count": 20,
-        "form-1-DELETE": "on",
-    }
-
-    formset = GenotypeFormSet(
-        data,
-        form_kwargs={"mutation_names": ["Mut-A"]},
-    )
-
-    assertFormSetError(
-        formset, form_index=None, field=None, errors=["All genotypes must be unique"]
-    )
+    assertFormSetError(formset, form_index=None, field=None, errors=[expected_msg])
