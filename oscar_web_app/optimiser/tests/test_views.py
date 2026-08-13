5b51e4029f6c1ae844a9fb47d7448f5888411faf
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-08-13 14:55:15 +0100

    add initial select_genotypes tests

--- oscar_web_app/optimiser/tests/test_views.py
+++ oscar_web_app/optimiser/tests/test_views.py
@@ -5,7 +5,15 @@ from django.urls import reverse
 from pytest_django.asserts import assertRedirects
 from pytest_django.asserts import assertTemplateUsed
 
+from oscar_web_app.optimiser.forms import GenotypeFormSet
 from oscar_web_app.optimiser.forms import LineForm
+
+
+@pytest.fixture(autouse=True)
+def colony_software_dev(settings):
+    """Force use of the DEV colony software"""
+
+    settings.COLONY_SOFTWARE = "DEV"
 
 
 @pytest.fixture
@@ -30,10 +38,38 @@ def test_select_line_post(logged_in_client, settings):
     line_id = 1
     assert line_id not in logged_in_client.session
 
-    settings.COLONY_SOFTWARE = "DEV"
     response = logged_in_client.post(
         reverse("optimiser:select_line"), {"line": line_id}
     )
 
     assert logged_in_client.session[str(line_id)] == "Line-A"
     assertRedirects(response, reverse("optimiser:select_genotypes", args=[line_id]))
+
+
+def test_select_genotypes_get(logged_in_client):
+    line_id = 1
+
+    response = logged_in_client.get(
+        reverse("optimiser:select_genotypes", args=[line_id])
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    formset = response.context["formset"]
+    assert isinstance(formset, GenotypeFormSet)
+    assert len(formset) == 1
+    assert sorted(formset[0].fields.keys()) == ["DELETE", "Mut-A", "count"]
+
+    assertTemplateUsed(
+        response=response, template_name="optimiser/select_genotypes.html"
+    )
+
+
+def test_select_line_with_no_mutations(logged_in_client, mocker):
+
+    mocker.patch(
+        "oscar_web_app.optimiser.colony_management.ColonyDev.get_line_mutations",
+        return_value=[],
+    )
+    response = logged_in_client.get(reverse("optimiser:select_genotypes", args=[1]))
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
