d70485cbd10c6a7e005b54e43565ea06c784ad94
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-08-13 15:56:06 +0100

    test 404 message for no mutations

--- oscar_web_app/optimiser/tests/test_views.py
+++ oscar_web_app/optimiser/tests/test_views.py
@@ -1,12 +1,15 @@
 from http import HTTPStatus
 
 import pytest
+from django.http import Http404
 from django.urls import reverse
+from pytest_django.asserts import assertRaisesMessage
 from pytest_django.asserts import assertRedirects
 from pytest_django.asserts import assertTemplateUsed
 
 from oscar_web_app.optimiser.forms import GenotypeFormSet
 from oscar_web_app.optimiser.forms import LineForm
+from oscar_web_app.optimiser.views import select_genotypes
 
 
 @pytest.fixture(autouse=True)
@@ -33,7 +36,7 @@ def test_select_line_get(logged_in_client):
     assertTemplateUsed(response=response, template_name="optimiser/select_line.html")
 
 
-def test_select_line_post(logged_in_client, settings):
+def test_select_line_post(logged_in_client):
 
     line_id = 1
     assert line_id not in logged_in_client.session
@@ -64,12 +67,14 @@ def test_select_genotypes_get(logged_in_client):
     )
 
 
-def test_select_line_with_no_mutations(logged_in_client, mocker):
+def test_select_line_with_no_mutations(rf, mocker):
 
     mocker.patch(
         "oscar_web_app.optimiser.colony_management.ColonyDev.get_line_mutations",
         return_value=[],
     )
-    response = logged_in_client.get(reverse("optimiser:select_genotypes", args=[1]))
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
+    request = rf.get(reverse("optimiser:select_genotypes", args=[1]))
+
+    with assertRaisesMessage(Http404, "No mutations found for chosen line"):
+        select_genotypes(request, line_id=1)
